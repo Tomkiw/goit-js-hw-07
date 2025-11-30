922efdc5b16cd0b7542268b4ad6adb29671257e2
Tomkiv_Ihor <igortomkiw3@gmail.com>
2025-11-30 14:38:27 +0200

    change

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,13 @@
-const customer = {
-  username: 'Mango',
-  balance: 24000,
-  discount: 0.1,
-  orders: ['Burger', 'Pizza', 'Salad'],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+const liSearch = document.querySelectorAll("li.item");
+console.log("🚀 ~ liSearch:", liSearch);
+//
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, 'Steak');
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+console.log("Nuber of categories:", liSearch.length);
+
+liSearch.forEach((li) => {
+  const h2 = li.querySelector("h2");
+  console.log("Category:", h2.textContent);
+
+  const ul = li.querySelectorAll("li");
+  console.log("Elenets:", ul.length);
+});
